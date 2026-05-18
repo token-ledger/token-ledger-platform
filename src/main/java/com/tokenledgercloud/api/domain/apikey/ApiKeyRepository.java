token-ledger/token-ledger-cloud-api

feat: API 키 발급 기능 구현 및 Flyway 도입

src/main/java/com/tokenledgercloud/api/domain/apikey/ApiKeyRepository.java
@@ -1,0 +1,10 @@
+package com.tokenledgercloud.api.domain.apikey;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface ApiKeyRepository extends JpaRepository<ApiKey, Long> {
+    Optional<ApiKey> findByApiKey(String apiKey);
+    List<ApiKey> findByMemberId(Long memberId);
+}

src/main/java/com/tokenledgercloud/api/dto/ApiKeyResponse.java
@@ -1,0 +1,20 @@
+package com.tokenledgercloud.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiKeyResponse {
+    private Long id;
+    private String apiKey;
+    private String name;
+    private LocalDateTime createdAt;
+    private boolean isActive;
+}

token-ledger/token-ledger-platform

src/main/java/com/tokenledgercloud/api/domain/apikey/ApiKeyRepository.java
@@ -1,0 +1,10 @@
+package com.tokenledgercloud.api.domain.apikey;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface ApiKeyRepository extends JpaRepository<ApiKey, String> {
+    Optional<ApiKey> findByApiKey(String apiKey);
+    List<ApiKey> findByMemberId(String memberId);
+}

src/main/java/com/tokenledgercloud/api/domain/apikey/dto/ApiKeyResponse.java
@@ -1,0 +1,20 @@
+package com.tokenledgercloud.api.domain.apikey.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiKeyResponse {
+    private String id;
+    private String apiKey;
+    private String name;
+    private LocalDateTime createdAt;
+    private boolean isActive;
+}